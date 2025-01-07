8096d18cfe56e47ddc6b6affd76ffb7bdb876eda
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2025-01-07 20:08:32 +0300

    isPalindrome  ve isPerfectNumber oluşturuldu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,26 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPerfectNumber(5));
+    }
+
+    public  static  boolean isPalindrome(int number){
+        String original = Integer.toString(number); //
+        String reversed = new StringBuilder(original).reverse().toString();
+        return original.equals(reversed);
+    }
+
+    public  static  boolean isPerfectNumber (int number){
+        if(number<0) {
+            return false;
+        }
+            int sum=0;
+      for(int i=1; i<number; i++){
+
+          if(number%i==0) {
+              sum+=i;
+          }
+      }
+      return sum==number;
     }
 }
